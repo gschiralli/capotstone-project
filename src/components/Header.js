575395a411dc0827aaf765b44582c57a73f3a2fb
Gianni Schiralli <gschiralli@hotmail.com>
2023-02-19 23:00:28 -0500

    basic structure completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav';
+
+const Header = () => {
+  return (
+    <header>
+      <img src="" alt="company logo" />
+      <Nav/>
+    </header>
+  )
+}
+
+export default Header
