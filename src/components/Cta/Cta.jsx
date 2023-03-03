67e8e8f05ccfb82fa7757bff3fb73e900ef1bb5b
Gianni Schiralli <gschiralli@hotmail.com>
2023-03-02 21:48:00 -0500

    updated css rules

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,6 +1,7 @@
 import "./cta.css";
 import food1 from "../../assets/food1.jpg";
 import Button from "./../Button/Button";
+import { Link } from "react-router-dom";
 const Cta = () => {
   return (
     <div className="container">
@@ -13,7 +14,9 @@ const Cta = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi
             enim excepturi, vero quaerat ipsum natus corrupti quibusdam.
           </p>
-          <Button title={"Reserve a Table"} />
+          <Link to={"/booking"}>
+            <Button title={"Reserve a Table"} />
+          </Link>
         </div>
         <div className="img-container">
           <img src={food1} alt="food" />
